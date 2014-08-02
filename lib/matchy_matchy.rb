475eec296efc259174094fc26fb4104b5380da56
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-02 16:04:06 -0400

    TDD Cycle 2: raised an error if the block's arity is not 1

--- lib/matchy_matchy.rb
+++ lib/matchy_matchy.rb
@@ -1,8 +1,10 @@
 module MatchyMatchy
   NO_BLOCK_GIVEN_ERROR = 'Block must be given to satisfy match'
+  REQUIRED_PARAM_ERROR = 'Block must accept matcher as parameter'
 
-  def self.match(value)
+  def self.match(value, &b)
     fail NO_BLOCK_GIVEN_ERROR unless block_given?
+    fail REQUIRED_PARAM_ERROR unless b.arity == 1
   end
 
 end
--- spec/lib/matchy_matchy_spec.rb
+++ spec/lib/matchy_matchy_spec.rb
@@ -10,6 +10,12 @@ describe MatchyMatchy do
       specify { expect { subject }.to raise_error('Block must be given to satisfy match') }
     end
 
+    context 'when given a value and a block that does not accept parameters' do
+      subject { described_class.match(42) { } }
+
+      specify { expect { subject }.to raise_error('Block must accept matcher as parameter') }
+    end
+
   end
 
 end
